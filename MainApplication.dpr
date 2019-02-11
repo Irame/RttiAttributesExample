af71bce9d942b5ef285f46f63974c5ebaf84995a
Felix Keller <f.keller@prologic.eu>
2019-02-11 13:22:30 +0100

    Added example

---
+++ MainApplication.dpr
@@ -0,0 +1,36 @@
+﻿program MainApplication;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  SimpleShareMem,
+  Winapi.Windows,
+  System.Rtti,
+  System.SysUtils;
+
+type
+  TProcedure = procedure;
+
+var lib : THandle;
+    fx  : TProcedure;
+    ctx : TRttiContext;
+
+begin
+  ctx := TRttiContext.Create;
+
+  ctx.GetType(TStringBuilder.ClassInfo);
+
+  Writeln('LoadLibrary');
+  lib := LoadLibrary('Project1.dll');
+
+
+  fx := TProcedure( GetProcAddress(lib, 'FOO') );
+
+  Writeln('Foo');
+  fx();
+
+  Writeln('FreeLibrary');
+  FreeLibrary(lib);
+end.
---
+++ Unit1.pas
@@ -0,0 +1,73 @@
+﻿unit Unit1;
+
+interface
+
+uses System.Rtti;
+
+type MyAttribute = class(TCustomAttribute)
+
+  public
+    constructor Create;
+    destructor Destroy; override;
+end;
+
+type TBlabla = class
+  FTest : Integer;
+
+  [My] property Test : Integer read FTest;
+end;
+
+procedure Foo(); stdcall;
+
+
+implementation
+
+procedure Foo(); stdcall;
+var
+  ctx       : TRttiContext;
+  lType     : TRttiType;
+  lAttribute: TCustomAttribute;
+  lProperty : TRttiProperty;
+begin
+  Writeln('TRttiContext.Create');
+
+  ctx       := TRttiContext.Create;
+  lType     := ctx.GetType(TBlabla);
+  for lProperty in lType.GetProperties do begin
+    for lAttribute in lProperty.GetAttributes do begin
+      Writeln('Attribute found: ' + lAttribute.ToString);
+    end;
+  end;
+
+  Writeln('TRttiContext.Free');
+  ctx.Free();
+end;
+
+{ MyAttribute }
+
+constructor MyAttribute.Create;
+begin
+  inherited;
+
+  WriteLn('MyAttribute.Create');
+end;
+
+destructor MyAttribute.Destroy;
+begin
+
+  WriteLn('MyAttribute.Destroy');
+
+  inherited;
+end;
+
+
+initialization
+
+  WriteLn('Unit1.initialization');
+
+finalization
+
+  WriteLn('Unit1.finalization');
+
+
+end.
